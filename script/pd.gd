d8643ea1e3e1a3b8bffd9df67eadf714f6b7f8d0
Omnilios <omnilios123@gmail.com>
2024-07-18 08:45:17 -0400

    novement

---
+++ script/pd.gd
@@ -0,0 +1,57 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+var curr_dirr ="none"
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+
+func player_movement(delta):
+	if Input.is_action_pressed("move_right"):
+		curr_dirr="right"
+		play_anim(0)
+		velocity.x=SPEED
+		velocity.y=0
+	elif Input.is_action_pressed("move_left"):
+		curr_dirr="left"
+		play_anim(0)
+		velocity.x=-SPEED
+		velocity.y=0
+	elif Input.is_action_pressed("move_up"):
+		play_anim(0)
+		velocity.x=0
+		velocity.y=-SPEED
+	elif Input.is_action_pressed("move_down"):
+		play_anim(0)
+		velocity.x=0
+		velocity.y=SPEED
+	else:
+		play_anim(1)
+		velocity.x=0
+		velocity.y=0
+		
+	move_and_slide()
+	
+func play_anim(mode):
+	var dir=curr_dirr	
+	var anim = $AnimatedSprite2D
+	
+	if dir=="right":
+		anim.flip_h=false
+		if mode==0:
+			anim.play("moveRight")
+		elif mode==1:
+			anim.play("idle")
+	else:
+		anim.flip_h=true
+		if mode==0:
+			anim.play("moveRight")
+		elif mode==1:
+			anim.play("idle")
+		
+		
+			
