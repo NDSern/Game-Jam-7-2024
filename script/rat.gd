6967148ebdf982d77576a2e4a70ea3284bba2bd7
NDSern <potarau123@gmail.com>
2024-07-21 15:40:42 +0300

    rudimantory pathfinding and placeholder tilesets

--- script/rat.gd
+++ script/rat.gd
@@ -1,24 +1,30 @@
 extends CharacterBody2D
 
 
-var speed = 50
+@export var movement_speed = 50
 var player_tracked = false
 
-@export var player: Node2D
-@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+@export var target: Node2D
+@onready var navigation_agent_2d = $NavigationAgent2D
 
-#func _physics_process(_delta: float):
-	#var dir = to_local(nav_agent.get_next_path_position().normalized())
-	#velocity = dir * speed
-	#move_and_slide()
-	#
-	#if velocity.x != 0 or velocity.y != 0:
-		#$AnimatedSprite2D.play("running_right")
-		#if velocity.x < 0:
-			#$AnimatedSprite2D.play("running_left")
-	#
-#func path():
-	#nav_agent.target_position = player.global_position
+func _ready():
+	call_deferred("find_target")
+	pass
+
+func _physics_process(delta):
+	if target:
+		navigation_agent_2d.target_position = target.global_position
+	if navigation_agent_2d.is_navigation_finished():
+		return
+	var curr_rat_position = global_position
+	var next_path_position = navigation_agent_2d.get_next_path_position()
+	velocity = curr_rat_position.direction_to(next_path_position) * movement_speed
+	move_and_slide()
+	
+func find_target():
+	await get_tree().physics_frame
+	if target:
+		navigation_agent_2d.target_position = target.global_position
 
 func _on_detection_area_body_entered(body):
 	player_tracked = true
